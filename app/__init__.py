fae571f5f19ac7f66dcf7555439016013dd8aefe
tanujsaini123 <148194541+tanujsaini123@users.noreply.github.com>
2025-07-25 18:23:44 +0530

    Update __init__.py

--- app/__init__.py
+++ app/__init__.py
@@ -3,7 +3,7 @@ from pymongo import MongoClient
 
 def create_app():
     app = Flask(__name__)
-    app.config["MONGO_URI"] = "mongodb://mongo:27017/ecommerce"
+    app.config["MONGO_URI"] = "mongodb://mongo-db:27017/ecommerce"
 
     client = MongoClient(app.config["MONGO_URI"])
     app.db = client.get_database()
